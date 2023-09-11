11b9f5ad688a908185284f678dcb44999a471731
codeClown <zhaomengshou@126.com>
2023-09-11 18:06:37 +0800

    名可名，非常名
    Signed-off-by: codeClown <zhaomengshou@126.com>

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,21 @@
-import {defineConfig} from 'vite'
+import { UserConfig, ConfigEnv, loadEnv } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), '.', dir)
+}
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
-})
+export default ({ command, mode }: ConfigEnv): UserConfig => {
+  const root = process.cwd()
+
+  const env = loadEnv(mode, root)
+
+  // The boolean type read by loadEnv is a string. This function can be converted to boolean type
+  // const viteEnv = wrapperEnv(env)
+
+  return {
+    plugins: [react()],
+  }
+}
